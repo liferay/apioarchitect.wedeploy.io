0c1cf98497ba7befbd49147ce03a145b22d84307
Alejandro Hernández <info@alejandrohdezma.com>
2018-12-19 11:32:30 +0100

    Remove unused logos and move logos to `logos` folder

--- src/components/LayoutNav/LayoutNav.js
+++ src/components/LayoutNav/LayoutNav.js
@@ -52,7 +52,7 @@ class LayoutNav extends Component {
             <nav ref="navElement" className={styles}>
                 <div className="container-fluid container-fluid-max-lg">
                     <Link to="/" className="navbar-brand">
-                        <img className="logo mr-2" src="/images/logo.png" alt="" />
+                        <img className="logo mr-2" src="/images/logos/apio_architect.png" alt="" />
                         <span className="title align-middle">{process.env.PROJECT_NAME}</span>
                     </Link>
                     <ul className="navbar-nav ml-auto">
